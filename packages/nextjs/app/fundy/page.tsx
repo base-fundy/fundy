5d9fbabd906a0c7dda250a93bf5521682e497d9a
Cristian Șerb <mail@criss.dev>
2024-03-28 19:26:04 +0200

    Hero

--- packages/nextjs/app/fundy/page.tsx
+++ packages/nextjs/app/fundy/page.tsx
@@ -21,7 +21,7 @@ const FundyRound: NextPage = () => {
   const { writeAsync, isLoading } = useScaffoldContractWrite({
     contractName: "YourContract",
     functionName: "fundProjectsByIds",
-    args: [votedProjects],
+    args: [votedProjects.toString()],
     value: parseEther(fundingAmount.toString()),
     onBlockConfirmation: txnReceipt => {
       console.log("📦 Transaction blockHash", txnReceipt.blockHash);
@@ -29,40 +29,59 @@ const FundyRound: NextPage = () => {
   });
 
   return (
-    <div className="card card-compact w-64 bg-secondary text-primary-content shadow-xl m-4">
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">Funding round 1</h2>
-        <div className="card-actions items-center flex-col gap-1 text-lg">
-          <h2 className="font-bold m-0">All Projects</h2>
-          {isProjectsLoading ? (
-            <span className="loading loading-spinner"></span>
-          ) : (
-            <ul className="list-disc pl-4">
-              {projects?.map(project => (
-                <li key={project.id}>
-                  {`${project.id} ${project.name} - Balance: ${project.balance}`}
+    <>
+      <div className="hero min-h-screen" style={{ backgroundImage: "url(/hero.svg)" }}>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="p-6">
+        <h1 className="text-2xl">Funding round 1</h1>
+        <progress className="progress w-56" value="70" max="100"></progress>
+        {isProjectsLoading ? (
+          <span className="loading loading-spinner"></span>
+        ) : (
+          projects?.map(project => (
+            <div key={project.id} className="card lg:card-side bg-base-100 shadow-xl mb-6">
+              <figure>
+                <img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" />
+              </figure>
+              <div className="card-body">
+                <h3 className="card-title">{project.name}</h3>
+                <span>{project.balance.toString()}</span>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </p>
+                <div className="card-actions justify-end">
+                  <label>Vote</label>
                   <input
                     type="checkbox"
                     onClick={() => {
                       setVotedProjects([...votedProjects, project.id]);
                     }}
                   />
-                </li>
-              ))}
-            </ul>
-          )}
-          <input
-            type="number"
-            onChange={event => {
-              setFundingAmount(Number(event.target.value));
-            }}
-          />
-          <button className="btn btn-primary" onClick={() => writeAsync()} disabled={isLoading}>
-            {isLoading ? <span className="loading loading-spinner loading-sm"></span> : <>Fund</>}
-          </button>
-        </div>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+        <input
+          type="number"
+          onChange={event => {
+            setFundingAmount(Number(event.target.value));
+          }}
+        />
+        <button className="btn btn-primary" onClick={() => writeAsync()} disabled={isLoading}>
+          {isLoading ? <span className="loading loading-spinner loading-sm"></span> : <>Fund</>}
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
