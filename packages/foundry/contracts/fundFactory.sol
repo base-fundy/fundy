acb525086e73fc104447ad0cd7b82c5f7dac4464
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-03-28 18:59:26 +0200

    change solidity version

--- packages/foundry/contracts/fundFactory.sol
+++ packages/foundry/contracts/fundFactory.sol
@@ -1,27 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./FundingRound.sol";
 
 contract FundFactory is Ownable {
-    address[] private fundingRounds;
+    address[] private _fundingRounds;
     // Assuming the mUSDC address is constant and known at compile time
-    address private immutable mUSDCAddress;
+    address private immutable _i_mUSDCAddress;
 
     constructor(address _mUSDCAddress) {
-        mUSDCAddress = _mUSDCAddress;
+        _i_mUSDCAddress = _mUSDCAddress;
     }
 
     event FundingRoundCreated(address indexed fundingRoundAddress);
 
     function createFundingRound() external onlyOwner {
-        FundingRound newRound = new FundingRound(mUSDCAddress);
-        fundingRounds.push(address(newRound));
+        FundingRound newRound = new FundingRound(_i_mUSDCAddress);
+        _fundingRounds.push(address(newRound));
         emit FundingRoundCreated(address(newRound));
     }
 
     function getFundingRounds() external view returns (address[] memory) {
-        return fundingRounds;
+        return _fundingRounds;
     }
 }
