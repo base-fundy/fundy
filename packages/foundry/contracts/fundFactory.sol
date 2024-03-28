4c1ab4d7103f8245c5f4a35bccf81c414892d522
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-03-28 14:45:35 +0200

    added fundFactory and mockUSDC

---
+++ packages/foundry/contracts/fundFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./FundingRound.sol";
+
+contract FundFactory is Ownable {
+    address[] private fundingRounds;
+    // Assuming the mUSDC address is constant and known at compile time
+    address private immutable mUSDCAddress;
+
+    constructor(address _mUSDCAddress) Ownable(msg.sender) {
+        mUSDCAddress = _mUSDCAddress;
+    }
+
+    event FundingRoundCreated(address indexed fundingRoundAddress);
+
+    function createFundingRound() external onlyOwner {
+        FundingRound newRound = new FundingRound(mUSDCAddress);
+        fundingRounds.push(address(newRound));
+        emit FundingRoundCreated(address(newRound));
+    }
+
+    function getFundingRounds() external view returns (address[] memory) {
+        return fundingRounds;
+    }
+}
